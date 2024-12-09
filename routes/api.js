233f889d184a10b0d9de3047e43415fb4422052a
ri1ycoders <161517210+ri1ycoders@users.noreply.github.com>
2024-12-10 04:50:49 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,11 @@
 __path = process.cwd()
 let express = require('express');
-let db = require(__path + '/database/db');
+/*let db = require(__path + '/database/db');
 try {
 let zahirr = db.get("zahirr");
 } catch (e) {
 	console.log('')  
-}
+}*/
 let creator = "Putbotz"
 let axios = require('axios')
 let fs = require('fs')
